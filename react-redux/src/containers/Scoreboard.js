189d2cf9a25c6805780f833e4675dc93e14dea56
Danielle H <daheber@gmail.com>
2017-03-13 21:05:45 -0700

    player detail

--- react-redux/src/containers/Scoreboard.js
+++ react-redux/src/containers/Scoreboard.js
@@ -1,31 +1,38 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
-import * as PlayerActionCreators from '../actions/player';
 import { connect } from 'react-redux';
-import AddPlayerForm from '../components/AddPlayerForm';
-import Player from '../components/Player';
+import * as PlayerActionCreators from '../actions/player';
 import Header from '../components/Header';
+import Player from '../components/Player';
+import AddPlayerForm from '../components/AddPlayerForm';
+import PlayerDetail from '../components/PlayerDetail';
 
 class Scoreboard extends Component {
-
   static propTypes = {
     players: PropTypes.array.isRequired
   };
 
   render() {
-    const { dispatch, players } = this.props;
+    const { dispatch, players, selectedPlayerIndex } = this.props;
     const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch);
     const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch);
     const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch);
+    const selectPlayer = bindActionCreators(PlayerActionCreators.selectPlayer, dispatch);
+
+    let selectedPlayer;
+    if (selectedPlayerIndex !== -1) {
+      selectedPlayer = players[selectedPlayerIndex];
+    }
 
     const playerComponents = players.map((player, index) => (
-      <Player 
+      <Player
         index={index}
         name={player.name}
         score={player.score}
         key={player.name}
         updatePlayerScore={updatePlayerScore}
         removePlayer={removePlayer}
+        selectPlayer={selectPlayer}
       />
     ));
 
@@ -36,6 +43,10 @@ class Scoreboard extends Component {
           { playerComponents }
         </div>
         <AddPlayerForm addPlayer={addPlayer} />
+        
+        <div className="player-detail">
+          <PlayerDetail selectedPlayer={selectedPlayer}/>
+        </div>
       </div>
     );
   }
@@ -43,7 +54,8 @@ class Scoreboard extends Component {
 
 const mapStateToProps = state => (
   {
-    players: state
+    players: state.players,
+    selectedPlayerIndex: state.selectedPlayerIndex
   }
 );
 

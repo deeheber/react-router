9508d87564ee8f3488ee738d706df673cbb307df
Danielle Heberling <daheber@gmail.com>
2017-03-14 16:23:50 -0700

    search functionality

--- gif-search/src/App.js
+++ gif-search/src/App.js
@@ -25,6 +25,18 @@ export default class App extends Component {
       });
   }
 
+  performSearch = query => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&limit=24`)
+      .then(response => {
+        this.setState({
+          gifs: response.data.data
+        });
+      })
+      .catch( err => {
+        console.log('Error fetching and parsing data ', err);
+      });
+  }
+
   render() { 
     console.log(this.state.gifs);
     return (
@@ -32,11 +44,11 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
+            <SearchForm onSearch={this.performSearch} />      
           </div>   
         </div>    
         <div className="main-content">
-          <GifList data={this.state.gifs}/>
+          <GifList data={this.state.gifs} />
         </div>
       </div>
     );

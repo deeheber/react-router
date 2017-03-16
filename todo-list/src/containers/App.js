16d88e64d645c4c02745830ba09fa5fa7e5f4ac6
Danielle H <daheber@gmail.com>
2017-03-15 20:14:11 -0700

    initial html mockup

--- todo-list/src/containers/App.js
+++ todo-list/src/containers/App.js
@@ -4,8 +4,31 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        Hello from the app component.
+        <div>
+          <h1>Add item</h1>
+          <form>
+            <input type="text" />
+            <button>Add</button>
+          </form>
+        </div>
+        <div>
+          <h1>To Do</h1>
+          <ul>
+            <li><input type="checkbox"/>Stuff</li>
+            <li><input type="checkbox"/>Things</li>
+            <li><input type="checkbox"/>Other things</li>
+          </ul>
+        </div>
+        <div>
+          <h1>Complete</h1>
+          <ul>
+            <li><input type="checkbox" checked="true"/>Complete stuff</li>
+            <li><input type="checkbox" checked="true"/>Complete things</li>
+            <li><input type="checkbox"checked="true"/>Other things that are complete</li>
+          </ul>
+        </div>
       </div>
+
     );
   }
 }

823f1dd942428472e2bea1304147708f59e68ef7
Danielle H <daheber@gmail.com>
2017-03-15 19:37:12 -0700

    init commit for TODO

---
+++ todo-list/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './app.css';
+import App from './src/containers/App';
+
+render(
+  <App />,
+  document.getElementById('app')
+);

ec36476aa7b3d67256a2253d229bf13338cfce3f
Danielle H <daheber@gmail.com>
2017-05-01 19:36:48 -0700

    delete confirmation if a task is not yet marked complete

--- todo-list/src/components/Task.js
+++ todo-list/src/components/Task.js
@@ -3,7 +3,16 @@ import PropTypes from 'prop-types';
 
 const Task = props => {
   let isComplete;
-  props.complete ? isComplete = 'complete' : isComplete = ''
+  props.complete ? isComplete = 'complete' : isComplete = '';
+
+  function deleteConfirm() {
+    // TODO make this prettier than the standard confirm dialog
+    const confirmed = confirm('This task is not yet complete. Are you sure you want to delete?');
+
+    confirmed ? props.removeTask() : '';
+  }
+
+  let handleDelete = props.complete ?  props.removeTask : deleteConfirm;
 
   return(
     <li className="list-group-item">
@@ -13,8 +22,7 @@ const Task = props => {
         onChange={props.toggleCompletion}
       />
       <span className={isComplete}>{props.name}</span>
-      {/*TODO: delete confirmation if a task is not marked completed */}
-      <button className="btn btn-danger pull-right btn-xs" onClick={props.removeTask}>X</button>
+      <button className="btn btn-danger pull-right btn-xs" onClick={handleDelete}>X</button>
     </li>
   );
 };
